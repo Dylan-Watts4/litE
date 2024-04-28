bb3ba7f2a2efe78dec504dab0a4c73db0dfaf2a3
Dylan-Watts4 <dylanuk2001@hotmail.co.uk>
2024-04-28 03:23:57 +0100

    Let me tidy

--- src/test.cpp
+++ src/test.cpp
@@ -5,8 +5,7 @@
 //#include <GLFW/glfw3.h>
 
 int main () {
-    int x = 1 + 1;
-    Windowed window(700,700,"GG");
+    Windowed window(700,700,"Test");
     window.createWindow();
     return 0;
 }
